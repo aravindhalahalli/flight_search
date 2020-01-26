9e60e0c3e26c3f70bf6237d2754021b36bfb6007
aravindhalahalli <aravindhalahalli.81@gmail.com>
2020-01-26 20:03:36 +0530

    Updated the code with new working functions

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,9 +1,21 @@
-
 import React from 'react';
 import './Item.css';
 import logo from './ItemLogo.svg';
 
-let Item = ({ item, children }) => (
+let Item = ({ item, children }) => {
+  let onClickHandler =  (item) => {
+    
+    let text = "Origin : "+ item.originCity + "\nDestination : " + item.destCity + "\nDeparture Date : "
+                + item.departureDate + "\t Departuew Time : " + item.deptDateTime;
+    alert("Flight booked successfully :)\n"+ text)
+    //event.preventDefault();
+  }
+  
+  Item.propTypes = {
+    item: React.PropTypes.object.isRequired,
+    children: React.PropTypes.node
+  };
+  return(
   <div className="Item">
     <div className="Item-left">
         <h5>Price: {item.price}</h5>
@@ -18,22 +30,12 @@ let Item = ({ item, children }) => (
       <div className="Item-image" >
         <img src={logo} className="Item-logo" alt="logo" />
       </div>
-      <button className="Item-book" onClick={item.onClickHandler} type="submit">
+      <button className="Item-book" onClick={()=>onClickHandler(item)}>
         Book this flight
       </button>
       {children}
     </div>
   </div>
 )
-
-Item.onClickHandler =  (event) => {
-  alert("Flight booked successfully :)")
-  event.preventDefault();
 }
-
-Item.propTypes = {
-  item: React.PropTypes.object.isRequired,
-  children: React.PropTypes.node
-};
-
 export default Item;
